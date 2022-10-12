12fdc25adb5fd1760c09710602056d6caeb16644
Carlos Garcia <carlos.grh@outlook.com>
2022-10-12 07:42:42 -0500

    create main function

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+# usually pytest searches for files with the name test in it. This can be configures and modified.
+
+
+
+if __name__ == "__main__":
+    
+
+    def func(x):
+        return x + 1
+
+    def test_answer():
+        assert func(3) == 5
