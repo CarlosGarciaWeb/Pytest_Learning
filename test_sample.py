1c622e0a47e45026edc51edef20b9dddb0c7b296
Carlos Garcia <carlos.grh@outlook.com>
2022-10-12 07:51:57 -0500

    first test with pytest on file location

--- test_sample.py
+++ test_sample.py
@@ -2,11 +2,9 @@
 
 
 
-if __name__ == "__main__":
+
+
+def test_our_first_test() -> None:
+    assert 1 == 1
+
     
-
-    def func(x):
-        return x + 1
-
-    def test_answer():
-        assert func(3) == 5
